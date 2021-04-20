34a631bfc6583c48c443c7c91f9ac8a09cd0d4af
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-04-19 21:12:33 -0400

    scheisse

--- source/app.ts
+++ source/app.ts
@@ -7,11 +7,27 @@ export default class App
 {
     public static main(): void 
     {
-        var container = Root.compose();
-        var processor = container.get<IProcessor>(TYPES.IProcessor);
-        processor.fetchRecords();
-        processor.configurePipeline();
-        processor.runPipeline();
-        processor.finalize();
+        try
+        {
+            var container = Root.compose();
+            var processor = container.get<IProcessor>(TYPES.IProcessor);
+
+            if (!processor.fetchRecords())
+                throw new Error("Failed to fetch records");
+
+            if (!processor.configurePipeline())
+                throw new Error("Failed to configure pipeline");
+            
+            if (!processor.runPipeline())
+                throw new Error("Failed to run pipeline");
+            
+            if (!processor.finalize())
+                throw new Error("Failed to finalize processor");
+
+        } catch (ex: any)
+        {
+            console.error(ex.message);
+        }
+
     }
 }
