a114545ec5ad52d8e824b0235b5453473216e712
oliverandrews <jaysonphillipmitchell@gmail.com>
2021-04-19 22:18:04 -0400

    Oops

--- source/models/Record.ts
+++ source/models/Record.ts
@@ -3,7 +3,7 @@ import { Status } from '../enums/status';
 export class Record
 {
     public status: Status;
-    public data: String;
+    public data: string;
 
     constructor(status: Status, data: string){
         this.status = status;
